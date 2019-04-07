16790b5abb42b61bdfdc291d2241b2099b760c59
Haroldthe24th <deathpolm@gmail.com>
2019-04-07 07:21:02 +0200

    m

---
+++ db_query_delete.php
@@ -0,0 +1,12 @@
+<?php 
+   include_once("db_inc.php");
+    $id = $_POST["id_delete"];
+    if(isset($id)){
+       
+    $sql_query = "DELETE FROM operations WHERE id=$id";
+    $delete_query = $connection->query($sql_query);
+}
+    
+
+     ?>
+
